b091acb36feb20572641a9f251066c6b9fa86209
Marjan02 <kevinzevalja@gmail.com>
2023-03-05 17:01:55 +0700

    update validasi login register

--- app/src/main/java/com/marjannnnn/approdite/SignInActivity.kt
+++ app/src/main/java/com/marjannnnn/approdite/SignInActivity.kt
@@ -5,11 +5,13 @@ import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
@@ -19,120 +21,114 @@ import com.marjannnnn.approdite.databinding.ActivitySignInBinding
 class SignInActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivitySignInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        firebaseAuth = FirebaseAuth.getInstance()
-        val yellowColor = ContextCompat.getColor(this, R.color.yellow)
-
-        binding.toSignUp.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.button.setOnClickListener {
-            var email = binding.emailEt.text.toString().trim()
-            val password = binding.passET.text.toString().trim()
-            var valid = true
-
-            binding.emailLayout.setHelperTextColor(ColorStateList.valueOf(Color.RED))
-            binding.passwordLayout.setHelperTextColor(ColorStateList.valueOf(Color.RED))
-
-            try {
-                if (email.isEmpty()) {
-                    binding.emailLayout.boxStrokeColor = Color.RED
-                    binding.emailLayout.hintTextColor = ColorStateList.valueOf(Color.RED)
-                    binding.emailLayout.helperText = "Email is required"
-                    valid = false
-                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    binding.emailLayout.boxStrokeColor = Color.RED
-                    binding.emailLayout.hintTextColor = ColorStateList.valueOf(Color.RED)
-                    binding.emailLayout.helperText = "Please enter a valid email address"
-                    valid = false
-                } else {
-                    binding.emailLayout.helperText = ""
-                }
-
-                if (password.isEmpty()) {
-                    binding.passwordLayout.boxStrokeColor = Color.RED
-                    binding.passwordLayout.hintTextColor = ColorStateList.valueOf(Color.RED)
-                    binding.passwordLayout.helperText = "Password is required"
-                    valid = false
-                } else {
-                    binding.passwordLayout.helperText = ""
-                }
-
-                if (valid) {
-                    firebaseAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                Toast.makeText(this, "Login success!", Toast.LENGTH_SHORT).show()
-                                val intent = Intent(this, MainActivity::class.java)
-                                startActivity(intent)
-                            } else {
-                                val errorMessage = when (task.exception) {
-                                    is FirebaseAuthInvalidUserException -> "Invalid email address"
-                                    is FirebaseAuthInvalidCredentialsException -> "Invalid password"
-                                    is FirebaseNetworkException -> "Network error"
-                                    else -> task.exception
-                                }
-                                Toast.makeText(
-                                    this, "$errorMessage", Toast.LENGTH_LONG
-                                ).show()
-                            }
-                        }
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                Toast.makeText(this, "Error: ${e.message}", Toast.LENGTH_LONG).show()
-            }
-        }
-
-        binding.emailEt.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // Do nothing
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Do nothing
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                binding.emailLayout.boxStrokeColor = yellowColor
-                binding.emailLayout.hintTextColor = ColorStateList.valueOf(yellowColor)
-                binding.emailLayout.helperText = ""
-            }
-        })
-
-        binding.passET.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // do nothing
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // do nothing
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                val password = binding.passET.text.toString().trim()
-
-                binding.passwordLayout.boxStrokeColor = yellowColor
-                binding.passwordLayout.hintTextColor = ColorStateList.valueOf(yellowColor)
-                binding.passwordLayout.helperText = ""
-            }
-
-        })
-    }
+    private var yellowColor: Int = 0
 
     override fun onStart() {
         super.onStart()
-        if (firebaseAuth.currentUser != null) {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        firebaseAuth.currentUser?.let {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
+        setContentView(binding.root)
+
+        binding.toSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString().trim()
+            val password = binding.passET.text.toString().trim()
+            var isValid = true
+
+            validateEmail(email, binding.emailLayout)
+            validatePassword(password, binding.passwordLayout)
+
+            if (binding.emailLayout.error != null || binding.passwordLayout.error != null) {
+                isValid = false
+            }
+
+            if (isValid) {
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "Login success!", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        } else {
+                            val errorMessage = when (task.exception) {
+                                is FirebaseAuthInvalidUserException -> "Invalid email address"
+                                is FirebaseAuthInvalidCredentialsException -> "Invalid password"
+                                is FirebaseNetworkException -> "Network error"
+                                else -> task.exception?.message
+                            }
+                            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+                        }
+                    }
+            }
+        }
+
+        binding.emailEt.addTextChangedListener(
+            createTextWatcher(binding.emailLayout, ::validateEmail)
+        )
+        binding.passET.addTextChangedListener(
+            createTextWatcher(binding.passwordLayout, ::validatePassword)
+        )
+    }
+
+    private fun String.isValidEmail(): Boolean {
+        return !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
+
+    private fun validateEmail(email: String, emailLayout: TextInputLayout) {
+        if (email.isEmpty()) {
+            setError(emailLayout, "Email is required")
+        } else if (!email.isValidEmail()) {
+            setError(emailLayout, "Please enter a valid email address")
+        } else {
+            clearError(emailLayout)
+        }
+    }
+
+    private fun validatePassword(password: String, passwordLayout: TextInputLayout) {
+        if (password.isEmpty()) {
+            setError(passwordLayout, "Password is required")
+        } else {
+            clearError(passwordLayout)
+        }
+    }
+
+    private fun setError(textInputLayout: TextInputLayout, errorMessage: String) {
+        textInputLayout.error = errorMessage
+        textInputLayout.boxStrokeColor = Color.RED
+        textInputLayout.hintTextColor = ColorStateList.valueOf(Color.RED)
+    }
+
+    private fun clearError(textInputLayout: TextInputLayout) {
+        yellowColor = ContextCompat.getColor(this, R.color.yellow)
+        textInputLayout.error = null
+        textInputLayout.boxStrokeColor = yellowColor
+        textInputLayout.hintTextColor = ColorStateList.valueOf(yellowColor)
+    }
+
+    private fun createTextWatcher(
+        textInputLayout: TextInputLayout, validationFunction: (String, TextInputLayout) -> Unit
+    ): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrEmpty()) {
+                    validationFunction(s.toString(), textInputLayout)
+                } else {
+                    clearError(textInputLayout)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/marjannnnn/approdite/SignUpActivity.kt
+++ app/src/main/java/com/marjannnnn/approdite/SignUpActivity.kt
@@ -1,86 +1,144 @@
 package com.marjannnnn.approdite
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.marjannnnn.approdite.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivitySignUpBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private var yellowColor: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
         setContentView(binding.root)
 
-        firebaseAuth = FirebaseAuth.getInstance()
-
         binding.toSignIn.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SignInActivity::class.java))
         }
 
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString().trim()
             val pass = binding.passET.text.toString().trim()
             val confirmPass = binding.confirmPassEt.text.toString().trim()
+            var isValid = true
 
-            if (email.isEmpty()) {
-                binding.emailEt.error = "Email is required"
-                binding.emailEt.requestFocus()
-                return@setOnClickListener
+            validateEmail(email, binding.emailLayout)
+            validatePassword(pass, binding.passwordLayout)
+            validateConfirmPassword(confirmPass, binding.confirmPasswordLayout)
+
+            if (binding.emailLayout.error != null || binding.passwordLayout.error != null || binding.confirmPasswordLayout.error != null) {
+                isValid = false
             }
 
-            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                binding.emailEt.error = "Please enter a valid email address"
-                binding.emailEt.requestFocus()
-                return@setOnClickListener
+            if (isValid) {
+                firebaseAuth.createUserWithEmailAndPassword(email, pass)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this, "Email created successfully!", Toast.LENGTH_SHORT
+                            ).show()
+                            startActivity(Intent(this, SignInActivity::class.java))
+                        } else {
+                            Toast.makeText(
+                                this,
+                                "Failed to create email: ${task.exception?.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
             }
+        }
 
-            if (pass.isEmpty()) {
-                binding.passET.error = "Password is required"
-                binding.passET.requestFocus()
-                return@setOnClickListener
-            }
 
-            if (pass.length < 6) {
-                binding.passET.error = "Password should be at least 6 characters"
-                binding.passET.requestFocus()
-                return@setOnClickListener
-            }
+        binding.emailEt.addTextChangedListener(
+            createTextWatcher(binding.emailLayout, ::validateEmail)
+        )
+        binding.passET.addTextChangedListener(
+            createTextWatcher(binding.passwordLayout, ::validatePassword)
+        )
+        binding.confirmPassEt.addTextChangedListener(
+            createTextWatcher(
+                binding.confirmPasswordLayout, ::validateConfirmPassword
+            )
+        )
+    }
 
-            if (confirmPass.isEmpty()) {
-                binding.confirmPassEt.error = "Please confirm your password"
-                binding.confirmPassEt.requestFocus()
-                return@setOnClickListener
-            }
+    private fun String.isValidEmail(): Boolean {
+        return !TextUtils.isEmpty(this) && Patterns.EMAIL_ADDRESS.matcher(this).matches()
+    }
 
-            if (pass != confirmPass) {
-                binding.confirmPassEt.error = "Password is not matching"
-                binding.confirmPassEt.requestFocus()
-                return@setOnClickListener
-            }
+    private fun validateEmail(email: String, emailLayout: TextInputLayout) {
+        if (email.isEmpty()) {
+            setError(emailLayout, "Email is required")
+        } else if (!email.isValidEmail()) {
+            setError(emailLayout, "Please enter a valid email address")
+        } else {
+            clearError(emailLayout)
+        }
+    }
 
-            firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        this, "Email created successfully!", Toast.LENGTH_SHORT
-                    ).show()
-                    val intent = Intent(this, SignInActivity::class.java)
-                    startActivity(intent)
+    private fun validatePassword(password: String, passwordLayout: TextInputLayout) {
+        if (password.isEmpty()) {
+            setError(passwordLayout, "Password is required")
+        } else {
+            clearError(passwordLayout)
+        }
+    }
+
+    private fun validateConfirmPassword(
+        confirmPassword: String, confirmPasswordLayout: TextInputLayout
+    ) {
+        val password = binding.passET.text.toString().trim()
+        if (confirmPassword.isEmpty()) {
+            setError(confirmPasswordLayout, "Confirm Password is required")
+        } else if (confirmPassword != password) {
+            setError(confirmPasswordLayout, "Confirm Password should match Password")
+        } else {
+            clearError(confirmPasswordLayout)
+        }
+    }
+
+    private fun setError(textInputLayout: TextInputLayout, errorMessage: String) {
+        textInputLayout.error = errorMessage
+        textInputLayout.boxStrokeColor = Color.RED
+        textInputLayout.hintTextColor = ColorStateList.valueOf(Color.RED)
+    }
+
+    private fun clearError(textInputLayout: TextInputLayout) {
+        yellowColor = ContextCompat.getColor(this, R.color.yellow)
+        textInputLayout.error = null
+        textInputLayout.boxStrokeColor = yellowColor
+        textInputLayout.hintTextColor = ColorStateList.valueOf(yellowColor)
+    }
+
+    private fun createTextWatcher(
+        textInputLayout: TextInputLayout, validationFunction: (String, TextInputLayout) -> Unit
+    ): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrEmpty()) {
+                    validationFunction(s.toString(), textInputLayout)
                 } else {
-                    Toast.makeText(
-                        this,
-                        "Failed to create email: ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    clearError(textInputLayout)
                 }
             }
         }
     }
+
 }
